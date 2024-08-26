a97f78cbacb7cd662058045b8f5639fd5ca54750
Hoang Tien Dat <dathtse170150@fpt.edu.vn>
2024-08-26 20:08:00 +0700

    video 31 using variable, syntax js in HTML

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,23 @@
-  // () => {}
-    //component = html + css + jss
-    const MyComponent = () => {
-        return(
-          <div> dat & chat gpt update</div> //tag
-        );
-       
-      }
+// () => {}
+//component = html + css + jss
+const MyComponent = () => {
+    //   const datit = "Hoang Tien Dat";
+    //   const datit = 25;
+    // const datit = true;
+    // const datit = null;
+    const datit = [1, 2, 3];
+    // const datit = {
+    //     name: "Hoang Tien Dat",
+    //     age: 21
+
+    // }
+    return (
+        <>  
+            <div>{console.log("Hoang Tien Dat")}</div>
+           <div> {JSON.stringify(datit)} & chat gpt update</div>
+        </> //tag
+
+    );
+
+}
 export default MyComponent;
