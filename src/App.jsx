e9ebfb30b90c9bb864177eaaa9f0d06d7b7ec557
Hoang Tien Dat <dathtse170150@fpt.edu.vn>
2024-08-27 23:22:53 +0700

    video 41

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import reactLogo from './assets/react.svg';
 import { useState } from 'react';
 const App = () => {
   const [todoList, setTodoList] = useState([
-    
+    // { id: 1, name: "Learn react" },
+    // { id: 2, name: "Watching Youtube" }
   ]);
 
 
@@ -17,14 +18,14 @@ const App = () => {
   }
   const addNewTodo = (name) => {
     const newToDo = {
-      id: randomIntFromInterval(1,10000),
+      id: randomIntFromInterval(1, 10000),
       name: name
     }
     //syntax js spread syntax
     setTodoList([...todoList, newToDo])
     function randomIntFromInterval(min, max) { // min and max included
       return Math.floor(Math.random() * (max - min + 1) + min);
-      }
+    }
   }
 
   return (
@@ -35,15 +36,31 @@ const App = () => {
           <Todonew
             addNewTodo={addNewTodo}
           />
-          <TodoData
-            name={datto}
-            age={age}
-            data={data}
-            todoList={todoList}
-          />
-          <div className='todo-image'>
-            <img src={reactLogo} alt="" />
-          </div>
+           {/* {todoList.length > 0 &&
+            <TodoData
+              todoList={todoList}
+            />
+
+          }
+          {todoList.length === 0 &&
+            <div className='todo-image' style={{ display: 'flex', justifyContent: 'center', margin: '15px' }}>
+              <img src={reactLogo} alt="" />
+            </div>
+
+          } */ }
+          {
+            todoList.length > 0 ?
+              <TodoData
+                todoList={todoList}
+              />
+
+              :
+
+              <div className='todo-image' style={{ display: 'flex', justifyContent: 'center', margin: '15px' }}>
+                <img src={reactLogo} alt="" />
+              </div>
+          }
+
         </div>
       </div>
     </>
